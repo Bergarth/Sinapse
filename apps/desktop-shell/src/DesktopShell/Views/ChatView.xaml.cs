8b478d47dcc2340f67e61c87910c540273c5c3c2
Bergarth <enzodad@gmail.com>
2026-03-30 07:03:01 -0400

    Implement end-to-end placeholder chat flow

--- apps/desktop-shell/src/DesktopShell/Views/ChatView.xaml.cs
+++ apps/desktop-shell/src/DesktopShell/Views/ChatView.xaml.cs
@@ -1,14 +1,23 @@
+using DesktopShell.Services;
 using DesktopShell.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace DesktopShell.Views;
 
 public sealed partial class ChatView : UserControl
 {
-    public ChatViewModel ViewModel { get; } = new();
+    public ChatViewModel ViewModel { get; }
 
     public ChatView()
     {
+        ViewModel = new ChatViewModel(new DaemonConnectionService());
         InitializeComponent();
+    }
+
+    private async void SendMessage_Click(object sender, RoutedEventArgs e)
+    {
+        _ = sender;
+        await ViewModel.SendMessageAsync();
     }
 }
